4dd816516a33972f75f1a876edd9efe8d76bb7e8
Сергей Иванов < >
2017-02-16 19:41:04 +0300

    first commit

---
+++ ShortLinkBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Сергей
+ * Date: 16.02.2017
+ * Time: 19:40
+ */
+
+namespace AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ShortLinkBundle extends Bundle
+{
+}

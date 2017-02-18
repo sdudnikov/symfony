5dc56e0a0890cb16dd0a59efb0aacf914f844e67
Сергей Иванов < >
2017-02-18 15:07:38 +0300

    create pages

--- Controller/ShortController.php
+++ Controller/ShortController.php
@@ -8,16 +8,43 @@
 namespace ShortLinkBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 
 
 
 class ShortController extends Controller
 {
     public function indexAction() {
-        return $this->render('@short/default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        return $this->render('@short/default/index.html.twig');
+    }
+
+    public function getLinksAction() {
+        $baseUrl = $this->generateUrl('indexRoute', array(), UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $result = array(
+            array('origin_url' => 'https://www.yandex.ru/', 'alias_url' => $baseUrl.'ffsdfd'),
+            array('origin_url' => 'https://www.yandex.ru/', 'alias_url' => $baseUrl.'dddd')
+        );
+
+        return new JsonResponse($result);
+    }
+
+    public function generateAction(Request $request){
+        $data = $request->request->all();
+        $t = 5;
 
     }
+
+    public function redirectAction($url) {
+        if($url == "test"){
+            return $this->redirect('https://www.yandex.ru/');
+        }
+        return $this->render('@short/default/error.html.twig');
+    }
+
+
+
 }
